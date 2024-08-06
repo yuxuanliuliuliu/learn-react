82ed679bf555f43f057eb82a01e12b7e7031f0be
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2024-08-06 12:42:55 -0400

    sample webpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile.js'
+import Page from './components/context/show.js'
 
 export default function Home() {
   return (
-    <div className={styles.main}>
-        <Profile />
-    </div>
+    <Page className={styles.section}/>
   )
 }
