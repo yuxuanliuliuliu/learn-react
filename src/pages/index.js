69b7757d0c37b77b2cbf6c18e371935d32676cfc
Joydeep Mitra <joy@Joydeeps-MacBook-Pro.local>
2024-08-06 12:59:42 -0400

    resolved merges

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,7 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-<<<<<<< HEAD
 import Page from './components/context/show.js'
-=======
-import Profile from './components/profile'
->>>>>>> b1a4461fc46c9f39f6708a8d01bf25597088450e
 
 export default function Home() {
   return (
